66e99f109e92351a081b8e7fe919afcd4244cfa7
sbme123 <simonbrown.dev@gmail.com>
2022-11-09 12:43:34 +0000

    initial push

---
+++ script.js
@@ -0,0 +1,26 @@
+// const swup = new Swup(); // only this line when included with script tag
+
+// document.addEventListener("DOMContentLoaded", () => {
+//     console.log("It's complete")
+// });
+
+// swup.on('pageView', function() {
+//     dataLayer.push({
+//       event: 'VirtualPageview',
+//       virtualPageURL: window.location.pathname,
+//       virtualPageTitle: document.title
+//     });
+//   });
+  
+//   swup.on('contentReplaced', function() {
+//     swup.options.containers.forEach((selector) => {
+//       // load scripts for all elements with 'selector'
+//     });
+//   });
+
+barba.init({
+  // ...
+  sync: true, 
+
+  transitions: a
+})
